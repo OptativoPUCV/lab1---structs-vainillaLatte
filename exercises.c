a51a70314eccc807cfe9b852dd5c967ee4ad0a1b
Replit user <>
2023-03-16 15:36:22 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -69,7 +69,7 @@ Persona* crearPersona(char nombre[], char rut[], int edad) {
    Persona * p = (Persona *) malloc(sizeof(Persona));
    //asignar valores de entrada a los campos de p
   
-    strcpy(nombre, p->nombre);
+    //strcpy(nombre, p->nombre);
     strcpy(rut, p->rut);
     //p->edad=edad;
     
